b565c75cfa93c68f2b0e8c8a528bce5d01134790
SaiAshish9 <saiashish3760@gmail.com>
2020-06-18 14:06:24 +0530

    k

---
+++ blog/components/Posts/PostsList.vue
@@ -0,0 +1,42 @@
+<template>
+        <section class="post-list">
+      <PostPreview
+      id="1"
+      thumbnail="https://image.freepik.com/free-vector/smart-city-concepts_46706-682.jpg"
+      title="Hello there!"
+      previewText="This is my first post!"
+        />
+
+      <PostPreview
+      id="2"
+      thumbnail="https://image.freepik.com/free-vector/smart-city-concepts_46706-682.jpg"
+      title="Hello there!"
+      previewText="This is my second post!"
+        />
+
+    </section>
+</template>
+
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ blog/pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-page">
+    <h1>"About Me" Page!</h1>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
