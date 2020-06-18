26d58ba14d11c09f07246ed0d201b0bdfbe668ee
SaiAshish9 <saiashish3760@gmail.com>
2020-06-18 16:32:46 +0530

    added layouts

--- blog/components/Posts/PostsList.vue
+++ blog/components/Posts/PostsList.vue
@@ -1,22 +1,22 @@
 <template>
-        <section class="post-list">
-      <PostPreview
+  <section class="post-list">
+    <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://image.freepik.com/free-vector/smart-city-concepts_46706-682.jpg"
       title="Hello there!"
       previewText="This is my first post!"
-        />
+    />
 
-      <PostPreview
+    <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://image.freepik.com/free-vector/smart-city-concepts_46706-682.jpg"
       title="Hello there!"
       previewText="This is my second post!"
-        />
-
-    </section>
+    />
+  </section>
 </template>
-
 
 <script>
 import PostPreview from "@/components/Posts/PostPreview";
@@ -24,11 +24,16 @@ import PostPreview from "@/components/Posts/PostPreview";
 export default {
   components: {
     PostPreview
+  },
+  props:{
+  isAdmin: {
+    type: Boolean,
+    default: false
   }
-}
+  }
+
+};
 </script>
-
-
 
 <style scoped>
 .post-list {
---
+++ blog/pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+
+export default {
+  layout: "admin",
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Sai",
+        title: "My Post",
+        content: "NIce Post",
+        thumbnailLink:
+          "https://image.freepik.com/free-vector/smart-city-concepts_46706-682.jpg"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
